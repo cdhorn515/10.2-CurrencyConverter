3ba2f6903b522176362f573f49a614edc8260cf4
Christina Cornell <cdhorn515@gmail.com>
2017-07-25 19:31:52 -0400

    reworked code, added switch statement and HashMap

--- src/com/cdhorn/Main.java
+++ src/com/cdhorn/Main.java
@@ -3,9 +3,18 @@ package com.cdhorn;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
 
-        Money money = new Money(5.0);
-        money.dollarToEuro(5.0);
+        Money money = new Money("JPY", 5.0);
+        money.toEuro();
+
+        Money money2 = new Money("EUR", 54.6);
+        money2.toYen();
+
+        Money money3 = new Money("USD", 50.0);
+        money3.toBitCoin();
+
+        Money money4 = new Money("XBT", 23.0);
+        money4.toDollar();
+
     }
 }
--- src/com/cdhorn/Tests/ConversionCalculation.java
+++ src/com/cdhorn/Tests/ConversionCalculation.java
@@ -1,15 +1,17 @@
+//import org.junit.Test;
+//
 //package com.cdhorn.Tests;
 //
 //
-//import com.cdhorn.Dollar;
+//import com.cdhorn.Money;
 //import org.junit.Test;
 //import static org.junit.Assert.*;
 //
 //
 //public class ConversionCalculation {
 //   @Test
-//   Dollar converted = new Dollar();
-//   converted = dollar.dollarToYen(5.0);
+//   Money dollar = new Money();
+//   dollar = dollar.dollarToYen(5.0);
 //
 //   assertEquals(559.1, double actual, double epsilon);
 //}
